48980761c43ee1227f815443d0b7b7d3d6e791f4
Yufan You <ouuansteve@gmail.com>
2022-04-02 12:02:32 +0800

    fix(plausible): only track at codle.ouuan.moe

--- src/plausible.ts
+++ src/plausible.ts
@@ -1,14 +1,26 @@
 import { ref, watch } from 'vue';
 import Plausible from 'plausible-tracker';
 
-export const {
-  trackEvent,
-  trackPageview,
-  enableAutoOutboundTracking,
+const {
+  trackEvent: event,
+  trackPageview: pageView,
+  enableAutoOutboundTracking: outBound,
 } = Plausible({
   domain: 'codle.ouuan.moe',
   apiHost: 'https://plausible.ouuan.moe',
 });
+
+function checkHost<T extends(...args: never[]) => void>(fn: T) {
+  return (...args: Parameters<T>) => {
+    if (window.location.host === 'codle.ouuan.moe') {
+      fn(...args);
+    }
+  };
+}
+
+export const trackEvent = checkHost(event);
+export const trackPageview = checkHost(pageView);
+export const enableAutoOutboundTracking = checkHost(outBound);
 
 export const plausibleTracked = ref<boolean>(localStorage.getItem('plausible_ignore') !== 'true');
 watch(plausibleTracked, (tracked) => {
